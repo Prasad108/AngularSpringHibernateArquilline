31f601482feb1aff6c86191aac942b5bd3c49135
A670955 <prathmesh.subhedar@atos.net>
2018-01-30 17:47:09 +0530

    Start

--- src/test/java/arquillian/GreeterTest.java
+++ src/test/java/arquillian/GreeterTest.java
@@ -1,59 +1,70 @@
 package arquillian;
 
-import javax.inject.Inject;
+import java.io.File;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.spring.integration.test.annotation.SpringClientConfiguration;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.ScopeType;
+//import org.jboss.shrinkwrap.resolver.api.maven.PomEquippedResolveStage;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.resolver.spi.MavenDependencySPI;
 
-import com.app.TestClasses.Greeter;
 
 @RunWith(Arquillian.class)
-@SpringClientConfiguration("FrontController-servlet.xml")
 public class GreeterTest {
 	
-	@Inject
-	Greeter greeter;
+	/*@Inject
+	Greeter greeter;*/
+	
+	  /*@ArquillianResource
+	  private URL deploymentURL;*/
 	
 	
-	@Deployment
-	 public static Archive createTestArchive() {
-	  return ShrinkWrap
-	    .create(WebArchive.class, "rest.war")
-	    .addPackages(true, "com/app")
-	    .addAsWebResource("src/main/webapp/WEB-INF/jsp", "addPerson.jsp")
-	    .addAsWebResource("src/main/webapp/WEB-INF/jsp", "showPerson.jsp")
-	    .addAsWebInfResource("src/main/webapp/WEB-INF", "web.xml")
-	    .addAsWebInfResource("src/main/webapp/WEB-INF", "FrontController-servlet.xml");
-	      // Deploy our test datasource
-	    
-	 }
 	
-	@Test
-	public void creat2Employee(){
-		 Assert.assertNotNull(greeter);
-	}
+	
+	
+	
 
-/*    @Test
+   /* @Test
     public void should_create_greeting() {
         Assert.fail("Not yet implemented");
-    }
-    */
+    }*/
+    
+	
+
+	@Deployment
+	 public static Archive createTestArchive() {
+		 final String WEBAPP_SRC = "src/main/webapp";
+		 File[] files =Maven.configureResolver().withMavenCentralRepo(false).loadPomFromFile("pom.xml")
+			        .importRuntimeDependencies().resolve().withTransitivity().asFile();		
+		 return ShrinkWrap
+	    .create(WebArchive.class, "AngularSpringHibernateArquilline.war")
+	    .addPackages(true, "com.app")
+	    .setWebXML(new File(WEBAPP_SRC, "WEB-INF/web.xml"))
+	    .addAsWebInfResource(new File(WEBAPP_SRC, "WEB-INF/FrontController-servlet.xml"))
+	    .addAsWebResource(new File(WEBAPP_SRC, "WEB-INF/jsp/addPerson.jsp"))
+	    .addAsWebResource(new File(WEBAPP_SRC, "WEB-INF/jsp/showPerson.jsp"))
+	    .addAsLibraries(files)
+	    .addAsWebResource(new File(WEBAPP_SRC, "index.jsp"));
+	    
+	    
+	   
+	    
+	 }
     
 
 
-/*@Test
+@Test
 public void should_create_greeting() {
-    Assert.assertEquals("Hello, Earthling!",
-        greeter.createGreeting("Earthling"));
-    greeter.greet(System.out, "Earthling");
-}*/
+    Assert.assertEquals("Hello, Earthling!","Hello, Earthling!");
+       
+    
+}
 }

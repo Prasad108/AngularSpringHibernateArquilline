ffa92372ec6b9d5091baccdd0362c6033c5ade80
A670955 <prathmesh.subhedar@atos.net>
2018-01-31 18:16:22 +0530

    Wildfly Jboss working

--- src/test/java/arquillian/GreeterTest.java
+++ src/test/java/arquillian/GreeterTest.java
@@ -3,6 +3,7 @@ package arquillian;
 import java.io.File;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -13,8 +14,17 @@ import org.jboss.shrinkwrap.resolver.api.maven.ScopeType;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.resolver.spi.MavenDependencySPI;
+import org.json.JSONObject;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 
 @RunWith(Arquillian.class)
@@ -53,17 +63,37 @@ public class GreeterTest {
 	    .addAsWebResource(new File(WEBAPP_SRC, "WEB-INF/jsp/showPerson.jsp"))
 	    .addAsLibraries(files)
 	    .addAsWebResource(new File(WEBAPP_SRC, "index.jsp"));
-	    
-	    
-	   
-	    
-	 }
+	  	 }
     
 
 
 @Test
+@RunAsClient
 public void should_create_greeting() {
-    Assert.assertEquals("Hello, Earthling!","Hello, Earthling!");
+    
+	Response  response = 	post("http://localhost:8080/AngularSpringHibernateArquilline/GetAllPersonList");
+	
+	/* given().
+	    when().
+	        get("http://localhost:8080/AngularSpringHibernateArquilline/showPerson").
+	    then().
+	        assertThat().
+	        //spec(checkStatusCodeAndContentType).
+	    and().
+	        body("MRData.CircuitTable.Circuits.circuitId",hasSize(20));
+	}*/
+	
+	//.body("fname", equalTo("Prasad"));
+	
+	/*JSONObject JSONResponseBody = new JSONObject(response.asString());
+	 
+	 //Get the desired value of a parameter
+	 String result = JSONResponseBody.getString({key});
+	*/
+	String body = response.getBody().asString();
+	
+	String test=response.jsonPath().getString("fname[0]");
+	System.out.println(test);
        
     
 }
